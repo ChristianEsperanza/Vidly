f4d507bdf17391a4882ec612ac593ea99a0bbc76
ChristianEsperanza <christianesperanzaa@gmail.com>
2020-03-13 20:46:26 -0700

    Add project files.

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                //If url has the following pattern, the first part of URL is the 
+                //name of the controller, second is the action, the third is an ID 
+                //Which can be passed to that action
+                url: "{controller}/{action}/{id}", 
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
